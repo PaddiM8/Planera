df4fd3c42f429b9eab4ad62fe88817be61dcaaf8
PaddiM8 <bakk@tuta.io>
2023-07-01 23:37:31 +0200

    web: Make login form enhanced

--- web/src/routes/login/+page.svelte
+++ web/src/routes/login/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     import type {ActionData} from "../../../.svelte-kit/types/src/routes/login/$types";
     import FormErrors from "$lib/FormErrors.svelte";
 
     export let form : ActionData;
 </script>
 
-<form method="POST">
+<form method="POST" use:enhance>
     <FormErrors errors={form?.errors} />
     <input type="text" name="username" placeholder="Username..." value={form?.username ?? ""} />
     <input type="password" name="password" placeholder="Password..." />
